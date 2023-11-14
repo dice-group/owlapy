675816592f5ac27d8c0e254105c8e8d0d147c730
Alkid <alkid1baci@gmail.com>
2023-11-14 16:25:24 +0100

    added setup.py and pyproject.toml

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+setup(
+    name="owlapy",
+    description="Owlapy is loosely based on owlapi, successfully representing the main owl objects in python.",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=1.5.0",
+        "rdflib>=6.0.2",
+        "parsimonious>=0.8.1"],
+    author='Ontolearn Team',
+    author_email='caglardemir8@gmail.com',
+    url='https://github.com/dice-group/owlapy',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: Scientific/Engineering"],
+    python_requires='>=3.8',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
