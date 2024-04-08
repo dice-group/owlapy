ca50e59a3cd04b4d2f9020d48057427d957b4a4b
Caglar Demir <caglardemir8@gmail.com>
2024-04-08 17:00:14 +0200

    WIP: Refactoring

--- owlapy/has.py
+++ owlapy/has.py
@@ -1,5 +1,7 @@
-from typing import Protocol, ClassVar
+from typing import Protocol, ClassVar, TypeVar, Generic, Iterable
 from abc import ABCMeta, abstractmethod
+_T = TypeVar('_T')  #:
+
 class HasIndex(Protocol):
     """Interface for types with an index; this is used to group objects by type when sorting."""
     type_index: ClassVar[int]  #: index for this type. This is a sorting index for the types.
@@ -17,5 +19,24 @@ class HasIRI(metaclass=ABCMeta):
 
         Returns:
             The IRI of this object.
+        """
+        pass
+
+
+class HasOperands(Generic[_T], metaclass=ABCMeta):
+    """An interface to objects that have a collection of operands.
+
+    Args:
+        _T: Operand type.
+    """
+    __slots__ = ()
+
+    @abstractmethod
+    def operands(self) -> Iterable[_T]:
+        """Gets the operands - e.g., the individuals in a sameAs axiom, or the classes in an equivalent
+        classes axiom.
+
+        Returns:
+            The operands.
         """
         pass
