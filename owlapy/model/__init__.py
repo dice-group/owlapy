9e1e40ee0b4fa6db93a5da8e1cce51bf771df46a
Caglar Demir <caglardemir8@gmail.com>
2024-04-08 17:07:26 +0200

    owl_class_expression script created

--- owlapy/model/__init__.py
+++ owlapy/model/__init__.py
@@ -12,6 +12,7 @@ from owlapy._utils import MOVE
 from owlapy.owlobject import OWLObject, OWLAnnotationObject, OWLAnnotationSubject, OWLAnnotationValue
 from owlapy.iri import IRI
 from owlapy.has import HasIndex, HasIRI, HasOperands
+from owlapy.owl_class_expression import OWLClassExpression, OWLObjectComplementOf, OWLAnonymousClassExpression, OWLBooleanClassExpression, OWLPropertyRange, OWLDataRange
 
 MOVE(OWLObject, OWLAnnotationObject, OWLAnnotationSubject, OWLAnnotationValue, HasIRI, IRI)
 
@@ -21,117 +22,6 @@ _P = TypeVar('_P', bound='OWLPropertyExpression')  #:
 _R = TypeVar('_R', bound='OWLPropertyRange')  #:
 Literals = Union['OWLLiteral', int, float, bool, Timedelta, datetime, date, str]  #:
 
-
-class OWLPropertyRange(OWLObject, metaclass=ABCMeta):
-    """OWL Objects that can be the ranges of properties."""
-
-
-class OWLDataRange(OWLPropertyRange, metaclass=ABCMeta):
-    """Represents a DataRange in the OWL 2 Specification."""
-
-
-class OWLClassExpression(OWLPropertyRange):
-    """An OWL 2 Class Expression."""
-    __slots__ = ()
-
-    @abstractmethod
-    def is_owl_thing(self) -> bool:
-        """Determines if this expression is the built in class owl:Thing. This method does not determine if the class
-        is equivalent to owl:Thing.
-
-        Returns:
-            True if this expression is owl:Thing.
-        """
-        pass
-
-    @abstractmethod
-    def is_owl_nothing(self) -> bool:
-        """Determines if this expression is the built in class owl:Nothing. This method does not determine if the class
-        is equivalent to owl:Nothing.
-        """
-        pass
-
-    @abstractmethod
-    def get_object_complement_of(self) -> 'OWLObjectComplementOf':
-        """Gets the object complement of this class expression.
-
-        Returns:
-            A class expression that is the complement of this class expression.
-        """
-        pass
-
-    @abstractmethod
-    def get_nnf(self) -> 'OWLClassExpression':
-        """Gets the negation normal form of the complement of this expression.
-
-        Returns:
-            A expression that represents the NNF of the complement of this expression.
-        """
-        pass
-
-
-class OWLAnonymousClassExpression(OWLClassExpression, metaclass=ABCMeta):
-    """A Class Expression which is not a named Class."""
-
-    def is_owl_nothing(self) -> bool:
-        # documented in parent
-        return False
-
-    def is_owl_thing(self) -> bool:
-        # documented in parent
-        return False
-
-    def get_object_complement_of(self) -> 'OWLObjectComplementOf':
-        # documented in parent
-        return OWLObjectComplementOf(self)
-
-    def get_nnf(self) -> 'OWLClassExpression':
-        # documented in parent
-        from owlapy.util import NNF
-        return NNF().get_class_nnf(self)
-
-
-class OWLBooleanClassExpression(OWLAnonymousClassExpression, metaclass=ABCMeta):
-    """Represent an anonymous boolean class expression."""
-    __slots__ = ()
-    pass
-
-
-class OWLObjectComplementOf(OWLBooleanClassExpression, HasOperands[OWLClassExpression]):
-    """Represents an ObjectComplementOf class expression in the OWL 2 Specification."""
-    __slots__ = '_operand'
-    type_index: Final = 3003
-
-    _operand: OWLClassExpression
-
-    def __init__(self, op: OWLClassExpression):
-        """
-        Args:
-            op: Class expression to complement.
-        """
-        self._operand = op
-
-    def get_operand(self) -> OWLClassExpression:
-        """
-        Returns:
-            The wrapped expression.
-        """
-        return self._operand
-
-    def operands(self) -> Iterable[OWLClassExpression]:
-        # documented in parent
-        yield self._operand
-
-    def __repr__(self):
-        return f"OWLObjectComplementOf({repr(self._operand)})"
-
-    def __eq__(self, other):
-        if type(other) is type(self):
-            return self._operand == other._operand
-        return NotImplemented
-
-    def __hash__(self):
-        return hash(self._operand)
 
 
 class OWLNamedObject(OWLObject, HasIRI, metaclass=ABCMeta):
