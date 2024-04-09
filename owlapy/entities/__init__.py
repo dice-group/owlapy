76ddc99741c0c5832a2d628f3bf592139d1c990a
Caglar Demir <caglardemir8@gmail.com>
2024-04-09 20:19:52 +0200

    owl entities module created

---
+++ owlapy/entities/__init__.py
@@ -0,0 +1,8 @@
+# https://www.w3.org/TR/owl2-syntax/#Entities.2C_Literals.2C_and_Anonymous_Individuals
+"""
+Entities are the fundamental building blocks of OWL 2 ontologies, and they define the vocabulary — the named terms — of an ontology.
+In logic, the set of entities is usually said to constitute the signature of an ontology.
+
+Classes, datatypes, object properties, data properties, annotation properties, and named individuals
+are entities, and they are all uniquely identified by an IR.
+"""
