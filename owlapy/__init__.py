c333d69e22abe06279e2b98eeae5690345554013
Alkid <alkid1baci@gmail.com>
2024-04-17 15:50:12 +0200

    Corrections

--- owlapy/__init__.py
+++ owlapy/__init__.py
@@ -1,4 +1,4 @@
-# from .render import owl_expression_to_dl, owl_expression_to_manchester
-# from .parser import dl_to_owl_expression, manchester_to_owl_expression
-# from .converter import owl_expression_to_sparql
+from .render import owl_expression_to_dl, owl_expression_to_manchester
+from .parser import dl_to_owl_expression, manchester_to_owl_expression
+from .converter import owl_expression_to_sparql
 __version__ = '0.1.3'
