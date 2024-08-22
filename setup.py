e3c2663ef536019eed48cb6616af5be8b9b97d69
Caglar Demir <caglardemir8@gmail.com>
2024-08-22 12:02:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 setup(
     name="owlapy",
     description="OWLAPY is a Python Framework for creating and manipulating OWL Ontologies.",
-    version="1.2.0",
+    version="1.2.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={
