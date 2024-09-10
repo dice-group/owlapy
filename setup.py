d4638254d30612f6c47f165f99c407b75c8c9c3d
Alkid <alkid1baci@gmail.com>
2024-09-10 14:08:20 +0200

    version increase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 setup(
     name="owlapy",
     description="OWLAPY is a Python Framework for creating and manipulating OWL Ontologies.",
-    version="1.2.2",
+    version="1.3.0",
     packages=find_packages(),
     include_package_data=True,
     package_data={
