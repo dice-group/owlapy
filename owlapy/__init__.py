56441189499d74475ab6e1986dfc394bb4e208d1
Caglar Demir <caglardemir8@gmail.com>
2024-10-17 11:06:31 +0200

    Usage of redudandant aliases removed

--- owlapy/__init__.py
+++ owlapy/__init__.py
@@ -1,6 +1,13 @@
 from .render import owl_expression_to_dl, owl_expression_to_manchester
 from .parser import dl_to_owl_expression , manchester_to_owl_expression
 from .converter import owl_expression_to_sparql, owl_expression_to_sparql_with_confusion_matrix
-from .owl_ontology_manager import OntologyManager as OntologyManager
+from .owl_ontology_manager import OntologyManager
 
 __version__ = '1.3.1'
+
+__all__ = [
+    'owl_expression_to_dl', 'owl_expression_to_manchester',
+    'dl_to_owl_expression', 'manchester_to_owl_expression',
+    'owl_expression_to_sparql', 'owl_expression_to_sparql_with_confusion_matrix',
+    'OntologyManager'
+]
