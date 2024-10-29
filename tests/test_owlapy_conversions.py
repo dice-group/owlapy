8f78f1c2cb5fe9709b3bcfad029da9b0d64cc712
Luke Friedrichs <lukefriedrichs@gmail.com>
2024-10-29 15:10:04 +0100

    added exist. test with two entities

--- tests/test_owlapy_conversions.py
+++ tests/test_owlapy_conversions.py
@@ -4,7 +4,8 @@ from owlapy.owl_property import OWLObjectProperty
 from owlapy.owl_individual import OWLNamedIndividual
 from owlapy.parser import DLSyntaxParser
 from owlapy.render import DLSyntaxObjectRenderer
-from owlapy.class_expression import OWLObjectHasValue
+from owlapy.class_expression import OWLObjectHasValue, OWLObjectSomeValuesFrom, OWLObjectOneOf
+
 
 class TestOWLObjectHasValueConversions(unittest.TestCase):
     def test_render(self):
@@ -19,6 +20,25 @@ class TestOWLObjectHasValueConversions(unittest.TestCase):
         self.assertEqual(c, parser.parse_expression(rendered_c))
 
         self.assertEqual(rendered_c, "∃ hasChild.{Demir}")
+
+    def test_render_multiple_entities(self):
+        renderer = DLSyntaxObjectRenderer()
+        NS = "http://example.com/father#"
+        parser = DLSyntaxParser(NS)
+        has_child = OWLObjectProperty(IRI(NS, 'hasChild'))
+
+        c = OWLObjectSomeValuesFrom(property=has_child, filler=OWLObjectOneOf([
+            OWLNamedIndividual(IRI.create(NS, 'Demir')),
+            OWLNamedIndividual(IRI.create(NS, 'Luke'))]
+        ))
+
+        renderer_c = renderer.render(c)
+        self.assertEqual(renderer_c, "∃ hasChild.{Demir , Luke}")
+        self.assertEqual(c, parser.parse_expression(renderer_c))
+
+        
+
+    
 
 
 if __name__ == '__main__':
