0dbc2de9a5dcea2d9491d35b4767aeceda22eea0
Caglar Demir <caglardemir8@gmail.com>
2024-11-08 17:35:33 +0100

    version incremanted

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 setup(
     name="owlapy",
     description="OWLAPY is a Python Framework for creating and manipulating OWL Ontologies.",
-    version="1.3.2",
+    version="1.3.3",
     packages=find_packages(),
     include_package_data=True,
     package_data={'owlapy': ['jar_dependencies/*.jar'],},
