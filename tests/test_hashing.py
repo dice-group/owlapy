86787928747a190f9632844f0e09c5bd8d516859
Caglar Demir <caglardemir8@gmail.com>
2024-11-08 17:04:03 +0100

    el concept construct tested via hashing

--- tests/test_hashing.py
+++ tests/test_hashing.py
@@ -1,12 +1,19 @@
-from owlapy.class_expression import OWLClass, OWLObjectIntersectionOf
 from owlapy.owl_property import OWLObjectProperty
-from owlapy.class_expression import OWLObjectSomeValuesFrom
+from owlapy.class_expression import OWLObjectSomeValuesFrom, OWLObjectAllValuesFrom
 from owlapy.converter import owl_expression_to_sparql
 from owlapy.render import owl_expression_to_dl
 from owlapy.iri import IRI
-from owlapy.class_expression import OWLObjectUnionOf, OWLObjectIntersectionOf
+from owlapy.class_expression import OWLClass, OWLObjectUnionOf, OWLObjectIntersectionOf
+
 class TestHashing:
-    def test_simple(self):
+
+    def test_el_description_logic_hash(self):
+        """
+        EL allows complex concepts of the following form:
+        C := \top | A | C1 u C2 | \existr.C
+        where A is a concept and r a role name.
+        For more, refer to https://www.emse.fr/~zimmermann/Teaching/KRR/el.html
+        """
         memory = dict()
         # An OWL Class can be used as a key in a dictionary.
         memory[OWLClass("http://example.com/father#A")] = OWLClass("http://example.com/father#A")
@@ -22,12 +29,25 @@ class TestHashing:
             # Since the order doesn't matter in an OWLObjectUnionOf the following also holds
             assert OWLObjectUnionOf((v, k)) in unions
 
-            # An OWLObjectUnionOf over two OWL Classes can be added into a set.
+            # This also works for intersections.
             intersections.add(OWLObjectIntersectionOf((k, v)))
             # Since the order doesn't matter in an OWLObjectUnionOf the following also holds
             assert OWLObjectIntersectionOf((v, k)) in intersections
         # OWLObjectUnionOf and OWLObjectIntersectionOf can also be used as keys
         for i in unions | intersections:
             memory[i]=i
+        for k, v in memory.items():
+            assert k == v
+
+        atomic_concepts={OWLClass("http://example.com/father#A"),OWLClass("http://example.com/father#B")}
+        properties={OWLObjectProperty("http://example.com/society#hasChild")}
+        memory = dict()
+        for ac in atomic_concepts:
+            for op in properties:
+                # OWLObjectSomeValuesFrom can be used as a key.
+                memory[OWLObjectSomeValuesFrom(property=op, filler=ac)] = OWLObjectSomeValuesFrom(property=op, filler=ac)
+                # TODO: https://github.com/dice-group/owlapy/issues/103
+                # memory[OWLObjectAllValuesFrom(property=op, filler=ac)] = OWLObjectAllValuesFrom(property=op, filler=ac)
+
         for k, v in memory.items():
             assert k == v
