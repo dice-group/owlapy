38854c795c69bb448258115109bf86e00070a6c4
Pritilata Saha <pritilata19saha95@gmail.com>
2025-10-05 16:16:58 +0200

    CoNeuralReasoner eval code

--- examples/retrieval_eval_nir.py
+++ examples/retrieval_eval_nir.py
@@ -1,15 +1,16 @@
 """
 How to run:
-CUDA_VISIBLE_DEVICES=3 python examples/retrieval_eval_nir.py --path_kg KGs/NewFamily/kb/ontology.owl
+CUDA_VISIBLE_DEVICES=3 python examples/retrieval_eval_nir_new.py --output_dir KGs/results/carcinogenesis --path_kg KGs/carcinogenesis/kb/ontology.owl --path_kge_model KGs/carcinogenesis/embeddings  --device gpu --dataset_dir KGs/carcinogenesis --path_report KGs/results/carcinogenesis/cqd_results.csv
 """
 from owlapy.owl_reasoner import StructuralReasoner, EBR
 from owlapy.owl_ontology import Ontology, NeuralOntology
 from owlapy.utils import jaccard_similarity, f1_set_similarity
-from owlapy.parser import DLSyntaxParser
 import json
 import time
 from typing import Tuple, Set
 import pandas as pd
+from owlapy.render import DLSyntaxObjectRenderer
+from owlapy.parser import DLSyntaxParser
 from owlapy import owl_expression_to_dl
 from itertools import chain
 from argparse import ArgumentParser
@@ -22,42 +23,6 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.colheader_justify', 'left')
 pd.set_option('display.expand_frame_repr', False)
-def dataframe_to_latex(df, caption="Table Caption", label="tab:table_label", decimals=3):
-    """
-    Converts a pandas DataFrame to a LaTeX table.
-
-    Args:
-        df (pd.DataFrame): The DataFrame to convert.
-        caption (str): The table caption.
-        label (str): The table label.
-        decimals (int): Number of decimal places to round to.
-
-    Returns:
-        str: The LaTeX table string.
-    """
-
-    latex_str = "\\begin{table}[htbp]\n"
-    latex_str += "\\centering\n"
-    latex_str += "\\caption{" + caption + "}\n"
-    latex_str += "\\label{" + label + "}\n"
-    latex_str += "\\resizebox{\\textwidth}{!}{\\begin{tabular}{" + "c" * (df.shape[1] + 1) + "}\n"  # +1 for the index column
-    latex_str += "\\toprule\n"
-
-    # Header row
-    header = ["Type"] + list(df.columns)
-    latex_str += " & ".join(header) + " \\\\\n"
-    latex_str += "\\midrule\n"
-
-    # Data rows
-    for index, row in df.iterrows():
-        row_str = [index] + [f"{val:.{decimals}f}" if isinstance(val, float) else str(val) for val in row]
-        latex_str += " & ".join(row_str) + " \\\\\n"
-
-    latex_str += "\\bottomrule\n"
-    latex_str += "\\end{tabular}}\n"
-    latex_str += "\\end{table}"
-
-    return latex_str
 def make_safe_eval():
     row_counter = {"i": -1}  # mutable dict to hold state
 
@@ -117,9 +82,15 @@ def execute(args):
     # (2) Initialize Neural OWL Reasoner
     # Use EBR instead of TripleStoreNeuralReasoner
     if args.path_kge_model:
-        neural_ontology = NeuralOntology(path_neural_embedding=args.path_kge_model, batch_size=args.batch_size, device=args.device, gamma=args.gamma)
+        neural_ontology = NeuralOntology(path_neural_embedding=args.path_kge_model,
+                                         batch_size=args.batch_size, device=args.device,
+                                         gamma=args.gamma)
     else:
-        neural_ontology = NeuralOntology(path_neural_embedding=args.path_kg, train_if_not_exists=True, model= args.model, epochs = args.epochs, batch_size=args.batch_size, device=args.device, gamma=args.gamma)
+        neural_ontology = NeuralOntology(path_neural_embedding=args.path_kg,
+                                         train_if_not_exists=True, scoring_technique = 'KvsAll',
+                                         model=args.kg_model, epochs=args.epochs,
+                                         batch_size=args.batch_size, device=args.device,
+                                         gamma=args.gamma)
     neural_owl_reasoner = EBR(ontology=neural_ontology)
 
     # () Iterate over single OWL Class Expressions in ALCQIHO
@@ -131,13 +102,21 @@ def execute(args):
         concepts = json.load(f)
         concepts = list(map(expression_parser.parse, concepts))
     rows = []
-    for expression in (tqdm_bar := tqdm(concepts, position=0, leave=True)):
+    count = 0
+    reasoning_errors_path = os.path.join(args.output_dir, "reasoning_errors.txt")
+    for expression  in (tqdm_bar := tqdm(concepts, position=0, leave=True)):
         retrieval_y: Set[str]
         runtime_y: Set[str]
         # () Retrieve the true set of individuals and elapsed runtime.
-        retrieval_y, runtime_y = concept_retrieval(symbolic_kb, expression)
-        # () Retrieve a set of inferred individuals and elapsed runtime.
-        retrieval_neural_y, runtime_neural_y = concept_retrieval(neural_owl_reasoner, expression)
+        try:
+            retrieval_y, runtime_y = concept_retrieval(symbolic_kb, expression)
+            # () Retrieve a set of inferred individuals and elapsed runtime.
+            retrieval_neural_y, runtime_neural_y = concept_retrieval(neural_owl_reasoner, expression)
+        except Exception as e:
+            with open(reasoning_errors_path, "a") as err_file:
+                err_file.write(f"Expression: {owl_expression_to_dl(expression)} | Error: {e}\n")
+            count += 1
+            continue
         # () Compute the Jaccard similarity.
         jaccard_sim = jaccard_similarity(retrieval_y, retrieval_neural_y)
         # () Compute the F1-score.
@@ -150,6 +129,7 @@ def execute(args):
         "F1": f1_sim,
         "Runtime Benefits": runtime_y - runtime_neural_y,
         "Runtime Neural": runtime_neural_y,
+        "Runtime Symbolic": runtime_y,
         "Symbolic_Retrieval": retrieval_y,
         "Symbolic_Retrieval_Neural": retrieval_neural_y,})
 
@@ -157,6 +137,11 @@ def execute(args):
         tqdm_bar.set_description_str(
             f"Expression: {owl_expression_to_dl(expression)} | Jaccard Similarity:{jaccard_sim:.4f} | F1 :{f1_sim:.4f} | Runtime Benefits:{runtime_y - runtime_neural_y:.3f}"
         )
+    if count > 0:
+        print(f"\n⚠️ Skipped {count} expressions due to reasoning errors.")
+        with open(reasoning_errors_path, "a") as err_file:
+            err_file.write(f"\n⚠️ Skipped {count} expressions due to reasoning errors.\n")
+
     df = pd.DataFrame(rows)
     df.to_csv(args.path_report, index=False)
     print("\n\x1b[6;30;42mSuccessfully saved the results!\x1b[0m\n")
@@ -177,46 +162,35 @@ def execute(args):
 
     # () Extract numerical features
     numerical_df = df.select_dtypes(include=["number"])
-
-    # () Group by the type of OWL concepts
-    df_g = df.groupby(by="Type")
-    print(df_g["Type"].count())
-
     # () Compute mean of numerical columns per group
-    mean_df = df_g[numerical_df.columns].mean()
+    mean_df = df[numerical_df.columns].mean()
     print(mean_df)
-    ## Write results as LaTex table
-    model_name = args.model.capitalize() if args.model.lower() not in ["lstm",
-                                                                       "gru"] else args.model.upper()
-    dataset_name = " ".join(list(map(str.capitalize,
-                                     [name for name in args.dataset_dir.split("/") if name.strip()][
-                                         -1].split("_"))))
-    dataset_name_lower = "_".join(list(map(str.lower, dataset_name.split(" "))))
-    latex_str = dataframe_to_latex(mean_df, caption=args.caption,
-                                   label="tab:" + model_name.lower() + "_" + dataset_name_lower)
-    with open(os.path.join(args.output_dir, f"{args.model.lower()}_latex_results.txt"), "w") as f:
-        f.write(latex_str)
+
+    def round_to_4(x):
+        return f"{x:.4f}"
+
+    with open(os.path.join(args.output_dir, f"{args.model.lower()}_avg_results.txt"), "w") as f:
+        f.write(" & ".join(list(numerical_df.columns)) + "\n")
+        f.write(" & ".join(list(map(round_to_4, mean_df.values.tolist()))) + "\n")
+
     return jaccard_sim, f1_sim
 
 def get_default_arguments():
     parser = ArgumentParser()
-    parser.add_argument("--model", type=str, default="DeCaL")
-    parser.add_argument("--output_dir", type=str, default="KGs/NewFamily/results")
-    parser.add_argument("--path_kg", type=str, default="KGs/NewFamily/kb/ontology.owl")
+    parser.add_argument("--kg_model", type=str, default="DeCaL")
+    parser.add_argument("--model", type=str, default="CQD")
+    parser.add_argument("--output_dir", type=str, default=None)
+    parser.add_argument("--path_kg", type=str, default=None)
     parser.add_argument("--caption", type=str, default="Retrieval Results")
     parser.add_argument("--path_kge_model", type=str, default=None)
     parser.add_argument("--gamma", type=float, default=0.9)
     parser.add_argument("--seed", type=int, default=1)
-    parser.add_argument("--ratio_sample_nc", type=float, default=1, help="To sample OWL Classes.")
-    parser.add_argument("--ratio_sample_object_prop", type=float, default=1, help="To sample OWL Object Properties.")
     parser.add_argument("--min_jaccard_similarity", type=float, default=0.0, help="Minimum Jaccard similarity to be achieve by the reasoner")
-    parser.add_argument("--num_nominals", type=int, default=10, help="Number of OWL named individuals to be sampled.")
     parser.add_argument("--batch_size", type=int, default=256, help="Batch size for the KGE model.")
-    parser.add_argument("--epochs", type=int, default=1, help="Number of epochs to train the KGE model if not provided.")
-    parser.add_argument("--device", type=str, default="cpu", help="Device to use for the KGE model.")
-    parser.add_argument("--dataset_dir", type=str, default="KGs/NewFamily", help="Directory containing test_data.json")
-    # H is obtained if the forward chain is applied on KG.
-    parser.add_argument("--path_report", type=str, default="ALCQHI_Retrieval_Results.csv")
+    parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train the KGE model if not provided.")
+    parser.add_argument("--device", type=str, default="gpu", help="Device to use for the KGE model.")
+    parser.add_argument("--dataset_dir", type=str, default=None, help="Directory containing test_data.json")
+    parser.add_argument("--path_report", type=str, default=None)
     return parser.parse_args()
 
 if __name__ == "__main__":
